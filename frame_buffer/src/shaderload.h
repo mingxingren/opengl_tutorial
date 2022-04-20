8089e0883c3b170c606a0339dd10fca638d6f9fd
mamingkun <mamingkun3@gmail.com>
2022-04-21 00:11:16 +0800

    1. upload code

---
+++ frame_buffer/src/shaderload.h
@@ -0,0 +1,133 @@
+#pragma once
+#include <glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader {
+public:
+	unsigned int ID;
+
+public:
+	Shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath = nullptr) {
+		std::cout << std::filesystem::current_path() << std::endl;
+
+		std::string vertexCode;
+		std::string fragmentCode;
+		std::string geometryCode;
+		std::ifstream vShaderFile;
+		std::ifstream fShaderFile;
+		std::ifstream gShaderFile;
+
+		vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		gShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		try {
+			vShaderFile.open(vertexPath);
+			fShaderFile.open(fragmentPath);
+			std::stringstream vShaderStream, fShaderStream;
+
+			// read file's buffer contents into streams
+			vShaderStream << vShaderFile.rdbuf();
+			fShaderStream << fShaderFile.rdbuf();
+
+			// close file handlers
+			vShaderFile.close();
+			fShaderFile.close();
+			
+			vertexCode = vShaderStream.str();
+			fragmentCode = fShaderStream.str();
+
+			if (geometryPath != nullptr) {
+				gShaderFile.open(geometryPath);
+				std::stringstream gShaderStream;
+				gShaderStream << gShaderFile.rdbuf();
+				gShaderFile.close();
+				geometryCode = gShaderStream.str();
+			}
+
+		}
+		catch (std::ifstream::failure& e) {
+			std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ: " << e.what() << std::endl;
+		}
+
+		const char* vShaderCode = vertexCode.c_str();
+		const char* fShaderCode = fragmentCode.c_str();
+
+		unsigned int vertex, fragment;
+		// vertex shader
+		vertex = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertex, 1, &vShaderCode, NULL);
+		glCompileShader(vertex);
+		checkCompileErrors(vertex, "VERTEX");
+		// fragment Shader
+		fragment = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragment, 1, &fShaderCode, NULL);
+		glCompileShader(fragment);
+		checkCompileErrors(fragment, "FRAGMENT");
+
+		unsigned int geometry;
+		if (geometryPath != nullptr) {
+			const char* gShaderCode = geometryCode.c_str();
+			geometry = glCreateShader(GL_GEOMETRY_SHADER);
+			glShaderSource(geometry, 1, &gShaderCode, NULL);
+			glCompileShader(geometry);
+			checkCompileErrors(geometry, "GEOMETRY");
+		}
+
+		ID = glCreateProgram();
+		glAttachShader(ID, vertex);
+		glAttachShader(ID, fragment);
+		if (geometryPath != nullptr) {
+			glAttachShader(ID, geometry);
+		}
+
+		glLinkProgram(ID);
+		checkCompileErrors(ID, "PROGRAM");
+		glDeleteShader(fragment);
+		glDeleteShader(vertex);
+		if (geometryPath != nullptr) {
+			glDeleteShader(geometry);
+		}
+	}
+
+	void use() {
+		glUseProgram(ID);
+	}
+
+	void setInt(const std::string& name, int value) const {
+		glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+	}
+
+	void setMat4(const std::string& name, const glm::mat4& mat) const {
+		glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, false, &mat[0][0]);
+	}
+
+private:
+	void checkCompileErrors(GLuint shader, std::string type) {
+		GLint success;
+		GLchar infoLog[1024];
+		if (type != "PROGRAM") {
+			glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+			if (!success) {
+				glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+				std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+			}
+			else {
+				std::cout << "success: " << type << std::endl;
+			}
+		}
+		else {
+			glGetProgramiv(shader, GL_LINK_STATUS, &success);
+			if (!success) {
+				glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+				std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+			}
+			else {
+				std::cout << "success: " << type << std::endl;
+			}
+		}
+	}
+};
